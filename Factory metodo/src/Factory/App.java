3c67e3c7b31d8b66577d1e71b3f23963d577ddb9
JeffersonMolina-art <84147110+JeffersonMolina-art@users.noreply.github.com>
2022-09-18 14:49:40 -0600

    Factory Metodo

--- Factory metodo/src/Factory/App.java
+++ Factory metodo/src/Factory/App.java
@@ -6,8 +6,7 @@ public class App {
 
     public static void main(String[] args){
 
-
-        lavadora lavadoraUtilizada = Constructor.LavadoraSeleccionada("frontal");
+        lavadora lavadoraUtilizada = Constructor.LavadoraSeleccionada("superior");
         System.out.println("Tipo lavadora es: ");
         System.out.println(lavadoraUtilizada.getTipoCarga());
         lavadoraUtilizada.ponerMandos();
